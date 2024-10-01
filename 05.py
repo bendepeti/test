2b688a1f60c34edf3294014e0bc99df8f8556f98
Bende Peter <bende.peter.levente.2023f@ms.gdszeged.hu>
2024-10-01 08:38:16 +0200

    elso

---
+++ 05.py
@@ -0,0 +1,15 @@
+import random
+
+# beégetem
+# a = 5
+# b = 7
+
+# felhasználó adja meg 
+# a = int(input("kérem adja meg az a szamot"))
+# b = int(input("kérem adja meg az b szamot"))
+
+a = random.randint(2,10)
+b = random.randint(10,15)
+
+c = a+ b
+print(f" a = {a} a b = {b} : összegük egyenlő c-vel {c}")
